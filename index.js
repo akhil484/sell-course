dec9ded42fcabf51ac2f2417b3e25674e13b42f4
Akhil <sharma.akhil484@gmail.com>
2024-09-22 17:39:41 +0530

    create skeleton routes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.get('/login', function(req,res){
+
+});
+
+app.get('/signup', function(req,res){
+
+});
+
+app.get('/view-course', function(req,res){
+
+});
+
+app.get('/my-course', function(req,res){
+
+});
+
+app.get('/buy-course', function(req,res){
+
+});
+
+app.listen(3000)
